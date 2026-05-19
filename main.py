870015cc1456746df15edcf2a0673b9be82b6788
bustamartin <mbusta2112@gmail.com>
2026-05-19 12:49:37 +0200

    fungujici_streamlit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -8,21 +9,42 @@ endpoint = "https://budwise-brigadnici-resource.openai.azure.com/openai/v1"
 deployment_name = "gpt-5"
 api_key = os.getenv("AZURE_OPENAI_API_KEY")
 
-print("API key loaded:", api_key is not None)
+print("API key se načetl:", api_key is not None)
 
 client = OpenAI(
     base_url=endpoint,
-    api_key=api_key
+    api_key=api_key,
 )
 
-completion = client.chat.completions.create(
-    model=deployment_name,
-    messages=[
-        {
-            "role": "user",
-            "content": "What is the capital of France?",
-        }
-    ],
-)
+# ___________________
 
-print(completion.choices[0].message.content)
+st.title("Bosch bot")
+
+user_input = st.text_input("Zadej otázku pro Bosch bota")
+
+if user_input:
+    st.write("Vaše otázka:", user_input)
+    completion = client.chat.completions.create(
+        model=deployment_name,
+        messages=[
+            {
+                "role": "user",
+                "content": user_input,
+            }
+        ],
+    )
+    response = completion.choices[0].message.content
+    st.write("Bosch bot:", response)
+
+
+# completion = client.chat.completions.create(
+#    model=deployment_name,
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": "Jaký je dneska datum?",
+#         }
+#     ],
+# )
+#
+# print(completion.choices[0].message.content)
