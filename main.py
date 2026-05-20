11755e421bcbadb8d8ce7c299bea36729fe6b832
furisrobin <robinfuri@gmail.com>
2026-05-20 11:50:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import pandas as pd
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
-import sqlite3
 
-from pages import logs
+st.set_page_config(
+    page_title="Úvodní strana"
+)
 
 load_dotenv()
 
@@ -13,46 +13,47 @@ endpoint = "https://budwise-brigadnici-resource.openai.azure.com/openai/v1"
 deployment_name = "gpt-5"
 api_key = os.getenv("AZURE_OPENAI_API_KEY")
 
-print("API key se načetl:", api_key is not None)
-
 client = OpenAI(
     base_url=endpoint,
     api_key=api_key,
 )
 
-# Inicializace sqlite databáze (vytvoří soubor data.db v kořeni projektu)
-def init_db():
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS generated_webs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            firma TEXT,
-            obor TEXT,
-            styl TEXT,
-            slovo1 TEXT,
-            slovo2 TEXT,
-            text_kod TEXT,
-            vytvoreno TEXT
-        )
-    """)
-    conn.commit()
-    conn.close()
+st.title("Bosch bot")
 
-init_db()
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-# UI Aplikace (streamlit)
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-st.title("🤖 AI Generator Landing Pages")
-st.write("Zadejte parametry a AI vám vytvoří kompletní prodejní stránku na míru.")
+user_input = st.chat_input("Zadej otázku pro Bosch bota:")
 
-# Formulář pro uživatele
-with st.form("generator_form"):
-    firma = st.text_input("Název firmy:", placeholder="např. Káva z hor s.r.o.")
-    obor = st.text_input("Obor podnikání:", placeholder="např. Výběrová kávová zrna, pražírna")
-    styl = st.selectbox(
-        "Styl a atmosféra webu:",
-        ["Moderní a čistý (SaaS)", "Luxusní a elegantní", "Technologický a tmavý", "Hravý a barevný"]
+if user_input:
+    st.session_state.messages.append({
+        "role": "user",
+        "content": user_input
+    })
+
+    with st.chat_message("user"):
+        st.write(user_input)
+
+    completion = client.chat.completions.create(
+        model=deployment_name,
+        messages=[
+            {
+                "role": "user",
+                "content": user_input,
+            }
+        ],
     )
-    
-    submit_button = st.form_submit_button("Vygenerovat web")
+
+    response = completion.choices[0].message.content
+
+    st.session_state.messages.append({
+        "role": "assistant",
+        "content": response
+    })
+
+    with st.chat_message("assistant"):
+        st.write(response)
